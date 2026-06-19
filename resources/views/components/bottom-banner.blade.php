4bfb6ceafefd4d2365c45008a8d4418027f1433a
namansainiii <namansaini960@gmail.com>
2026-06-19 15:50:35 +0530

    Update bottom-banner.blade.php

--- resources/views/components/bottom-banner.blade.php
+++ resources/views/components/bottom-banner.blade.php
@@ -2,7 +2,7 @@
 'heading' => 'Post your job listing now and find the perfect candidate.'])
 
 <section class="w-full">
-    <div class="bg-blue-800 text-white pl-4 pr-4 pt-2 pb-2 flex items-center justify-between flex-col md:flex-row gap-4 mt-4 mb-2">
+    <div class="bg-blue-800 text-white pl-4 pr-4 pt-2 pb-2 flex items-center justify-between flex-col md:flex-row gap-4 mt-4">
         <div>
             <h2 class="text-xl font-semibold">{{$title}}</h2>
             <p class="text-gray-200 text-lg mt-2">
