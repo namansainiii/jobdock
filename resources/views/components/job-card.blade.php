8db192ba20bc9b5094db669025ab83c8c54db748
namansainiii <namansaini960@gmail.com>
2026-06-22 22:12:23 +0530

    Update job-card.blade.php

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -32,5 +32,5 @@
         @endif
 
     </ul>
-    <a href="{{ route('jobs.show' , $job->id )}}" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">Details</a>
+    <a href="{{ route('jobs.show' , $job->id )}}" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">Job Details</a>
 </div>
