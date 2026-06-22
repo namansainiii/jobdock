d5f855981f491a78f23fe0de6399f41dfe3918e6
namansainiii <namansaini960@gmail.com>
2026-06-22 23:18:51 +0530

    changes

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,14 +1,14 @@
 {{-- @extends('layout')
 @section('content') --}}
 <x-layout>
-  <div class="bg-blue-900 h-24 mb-4 flex justify-center items-center"><x-search></x-search></div>
+  <div style="margin-top: -15px;" class="bg-blue-900 h-24 mb-4 flex justify-center items-center"><x-search></x-search></div>
   {{-- Back button --}}
   @if(request()->has('keywords') || request()->has('location'))
       <a href="{{ route('jobs.index') }}" style="background: gray;" class="hover:bg-gray-600 text-white px-4 py-2 rounded mb-4 inline-block ml-4">
           <i class="fa fa-arrow-left mr-1"></i> Back
       </a>
   @endif
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
     @forelse($jobs as $job)
       <x-job-card :job="$job"></x-job-card>
       {{-- <div style="text:bold"><a href="{{ route( 'jobs.show' , $job->id) }}">{{$job->title}}</a></div> --}}
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,19 @@
 {{-- @extends('layout')
 @section('content') --}}
 <x-layout>
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
        @forelse($jobs as $job)
            <x-job-card :job="$job"></x-job-card>
        @empty
            <p1>No job present</p1>
        @endforelse
     </div>
-    <a href="{{ route('jobs.index') }}" class="block text-xl text-center">
-        <i class="fa fa-arrow-alt-circle-right"></i>Show All Jobs</a>
+    <div class="flex justify-center">
+        <a href="{{ route('jobs.index') }}" class="inline-flex items-center px-6 py-4 bg-blue-700 hover:bg-blue-500 text-white text-sm font-semibold rounded-xl shadow-md transition-all duration-200">
+            <i class="fa fa-list"></i>
+            Show All Jobs
+        </a>
+    </div>
     {{-- <x-bottom-banner title="looking to hire?" heading="testing heading"/> --}}
     {{-- <x-bottom-banner /> --}}
 
