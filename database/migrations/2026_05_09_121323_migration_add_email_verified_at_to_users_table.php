be394e1326dbec813ee4833e21de484c6987d990
namansainiii <namansaini960@gmail.com>
2026-08-22 22:34:46 +0530

    Add safety checks to migrations for PostgreSQL

--- database/migrations/2026_05_09_121323_migration_add_email_verified_at_to_users_table.php
+++ database/migrations/2026_05_09_121323_migration_add_email_verified_at_to_users_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('email_verified_at')->nullable()->after('email');
-        });
+        if (!Schema::hasColumn('users', 'email_verified_at')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->timestamp('email_verified_at')->nullable()->after('email');
+            });
+        }
     }
 
     /**
--- database/migrations/2026_07_09_142427_add_resume_path_to_users_table.php
+++ database/migrations/2026_07_09_142427_add_resume_path_to_users_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('resume_path')->nullable()->after('avatar');
-        });
+        if (!Schema::hasColumn('users', 'resume_path')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('resume_path')->nullable()->after('avatar');
+            });
+        }
     }
 
     /**
